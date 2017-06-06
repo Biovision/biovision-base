d62c6c824386a56b8cc288bfcba82b7d7263536f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-06-07 02:34:29 +0300

    Add helpers to check if current user is in privilege group

--- lib/biovision/base/privilege_methods.rb
+++ lib/biovision/base/privilege_methods.rb
@@ -12,12 +12,23 @@ module Biovision
         ::UserPrivilege.user_has_privilege?(current_user, privilege_name)
       end
 
+      # @param [Symbol] group_name
+      def current_user_in_group?(group_name)
+        ::UserPrivilege.user_in_group?(current_user, group_name)
+      end
+
       protected
 
       # @param [Symbol] privilege_name
       def require_privilege(privilege_name)
         return if current_user_has_privilege?(privilege_name)
         handle_http_401("Current user has no privilege #{privilege_name}")
+      end
+
+      # @param [Symbol] group_name
+      def require_privilege_group(group_name)
+        return if current_user_in_group?(group_name)
+        handle_http_401("Current user is not in group #{group_name}")
       end
     end
   end
