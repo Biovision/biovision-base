1e0586785a0c88d138367cf76e850b9873c3e8b0
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-06-12 01:25:30 +0300

    Using breadcrubms and lists instead of table for agents and browsers

--- app/helpers/browsers_helper.rb
+++
@@ -1,7 +0,0 @@
-module BrowsersHelper
-  def browsers_for_select
-    options = [[t(:not_set), '']]
-    Browser.ordered_by_name.each { |browser| options << [browser.name, browser.id] }
-    options
-  end
-end
---
+++ app/helpers/tracking_helper.rb
@@ -0,0 +1,17 @@
+module TrackingHelper
+  # @param [Agent] entity
+  def admin_agent_link(entity)
+    link_to(entity.name, admin_agent_path(entity.id))
+  end
+
+  # @param [Browser] entity
+  def admin_browser_link(entity)
+    link_to(entity.name, admin_browser_path(entity.id))
+  end
+
+  def browsers_for_select
+    options = [[t(:not_set), '']]
+    Browser.ordered_by_name.each { |browser| options << [browser.name, browser.id] }
+    options
+  end
+end
