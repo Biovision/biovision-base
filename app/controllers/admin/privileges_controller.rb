b66a2691b426853647acea83b7eedc360fa598b4
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-06-15 03:40:30 +0300

    Remove concerns from privileges controller

--- app/controllers/admin/privileges_controller.rb
+++ app/controllers/admin/privileges_controller.rb
@@ -1,5 +1,33 @@
 class Admin::PrivilegesController < AdminController
   include LockableEntity
   include EntityPriority
-  include Biovision::Admin::Privileges
+
+  before_action :set_entity, except: [:index]
+
+  # get /admin/privileges
+  def index
+    @collection = Privilege.for_tree
+  end
+
+  # get /admin/privileges/:id
+  def show
+  end
+
+  # get /admin/privileges/:id/users
+  def users
+    @collection = @entity.users.page_for_administration(current_page)
+  end
+
+  protected
+
+  def restrict_access
+    require_privilege :administrator
+  end
+
+  def set_entity
+    @entity = Privilege.find_by(id: params[:id], deleted: false)
+    if @entity.nil?
+      handle_http_404("Cannot find non-deleted privilege #{params[:id]}")
+    end
+  end
 end
