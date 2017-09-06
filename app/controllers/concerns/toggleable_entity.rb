cabe9556ce96bf37cf4c2f4c0b3ab3518665780b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-09-07 01:45:39 +0300

    Check if entity is editable by current user before toggle

--- app/controllers/concerns/toggleable_entity.rb
+++ app/controllers/concerns/toggleable_entity.rb
@@ -1,7 +1,23 @@
 module ToggleableEntity
   extend ActiveSupport::Concern
 
+  # Toggle entity flag when allowed
   def toggle
-    render json: { data: @entity.toggle_parameter(params[:parameter].to_s) }
+    if allow_toggle?
+      render json: { data: @entity.toggle_parameter(params[:parameter].to_s) }
+    else
+      head :unauthorized
+    end
+  end
+
+  private
+
+  # If entity responds to #editable_by?, it should be editable to be toggled
+  def allow_toggle?
+    if @entity.respond_to?(:editable_by?)
+      @entity.editable_by?(current_user)
+    else
+      true
+    end
   end
 end
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -1,5 +1,5 @@
 module Biovision
   module Base
-    VERSION = '0.7.170823'
+    VERSION = '0.7.170907'
   end
 end
