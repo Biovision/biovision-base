a2c7517a9be12eed90b7adff0adeb6521021dc34
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-02-06 03:24:34 +0300

    Added polyfill for ParentNode.prepend()

--- app/assets/javascripts/biovision/base/biovision.js
+++ app/assets/javascripts/biovision/base/biovision.js
@@ -412,6 +412,38 @@ if (!Element.prototype.closest) {
 }
 
 /**
+ * ParentNode.prepend()
+ *
+ * IE
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/prepend
+ * @see https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/prepend()/prepend().md
+ */
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('prepend')) {
+            return;
+        }
+        Object.defineProperty(item, 'prepend', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function prepend() {
+                let argArr = Array.prototype.slice.call(arguments),
+                    docFrag = document.createDocumentFragment();
+
+                argArr.forEach(function (argItem) {
+                    let isNode = argItem instanceof Node;
+                    docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+                });
+
+                this.insertBefore(docFrag, this.firstChild);
+            }
+        });
+    });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+/**
  * ParentNode.append()
  *
  * IE 9+
