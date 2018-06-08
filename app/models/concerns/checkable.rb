406c767be6f36e0b0c689d027df60dc1bc1c9f80
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-06-08 15:37:09 +0300

    Added concern for instant check in models

---
+++ app/models/concerns/checkable.rb
@@ -0,0 +1,20 @@
+# Adds method for validating model in controllers
+#
+# @author Maxim Khan-Magomedov <maxim.km@gmail.com>
+module Checkable
+  extend ActiveSupport::Concern
+
+  included do
+    # @param [Integer] id
+    # @param [Hash] parameters
+    def self.instance_for_check(id, parameters)
+      if id.blank?
+        entity = new(parameters)
+      else
+        entity = find_by(id: id)
+        entity.assign_attributes(parameters)
+      end
+      entity
+    end
+  end
+end
