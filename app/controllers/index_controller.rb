78ba9ea1badddd489a57b69a8c463f3f633ea729
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-07-26 01:19:50 +0300

    see #36, saving referral link in cookie

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,19 @@
 class IndexController < ApplicationController
   # get /
   def index
-    @editable_page = EditablePage.find_localized('index', locale)
+    @editable_page = EditablePage.localized_page('index', locale)
+    check_referral_link if params.key?(:rl)
+  end
+
+  private
+
+  def check_referral_link
+    return if cookies['r']
+    cookies['r'] = {
+      value: param_from_request(:rl),
+      expires: 1.year.from_now,
+      domain: :all,
+      httponly: true
+    }
   end
 end
