f7b9555987da8fe5c88dc95e9e5bc1099cda51b8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-10-28 02:44:25 +0300

    Added icons for contact and registration components

--- app/models/code_type.rb
+++ app/models/code_type.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# Code type
+#
+# Attributes:
+#   - name [String]
+#   - slug [String]
 class CodeType < ApplicationRecord
   include RequiredUniqueName
   include RequiredUniqueSlug
@@ -6,6 +13,7 @@ class CodeType < ApplicationRecord
   NAME_LIMIT = 100
 
   validates_length_of :name, maximum: NAME_LIMIT
+  validates_length_of :slug, maximum: SLUG_LIMIT
 
   has_many :codes, dependent: :delete_all
 end
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.26.181028.0'
+    VERSION = '0.26.181028.1'
   end
 end
