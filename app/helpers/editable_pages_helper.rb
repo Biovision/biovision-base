43e944d899342abe1b6e2b06e28573989344691f
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-11-12 18:19:34 +0300

    Enhancements for editable blocks
    Added "edit" link
    Added "hd" image version into helper

--- app/helpers/editable_pages_helper.rb
+++ app/helpers/editable_pages_helper.rb
@@ -12,6 +12,7 @@ module EditablePagesHelper
   # @param [EditablePage] entity
   def editable_page_image_preview(entity)
     return '' if entity.image.blank?
+
     versions = "#{entity.image.preview_2x.url} 2x"
     image_tag(entity.image.preview.url, alt: entity.name, srcset: versions)
   end
@@ -19,13 +20,23 @@ module EditablePagesHelper
   # @param [EditablePage] entity
   def editable_page_image_medium(entity)
     return '' if entity.image.blank?
+
     versions = "#{entity.image.medium_2x.url} 2x"
     image_tag(entity.image.medium.url, alt: entity.name, srcset: versions)
+  end
+
+  # @param [EditablePage] entity
+  def editable_page_image_hd(entity)
+    return '' if entity.image.blank?
+
+    versions = "#{entity.image.large.url} 2x"
+    image_tag(entity.image.medium_2x.url, alt: entity.name, srcset: versions)
   end
 
   # @param [EditableBlock] entity
   def editable_block_image_preview(entity)
     return '' if entity.image.blank?
+
     versions = "#{entity.image.preview_2x.url} 2x"
     image_tag(entity.image.preview.url, alt: entity.name, srcset: versions)
   end
@@ -33,6 +44,7 @@ module EditablePagesHelper
   # @param [EditableBlock] entity
   def editable_block_image_medium(entity)
     return '' if entity.image.blank?
+
     versions = "#{entity.image.medium_2x.url} 2x"
     image_tag(entity.image.medium.url, alt: entity.name, srcset: versions)
   end
