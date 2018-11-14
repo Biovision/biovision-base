40dc6afd9dd7a36c3b28cb1e02b152110eb54deb
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-11-14 14:47:28 +0300

    Added simple file uploader

---
+++ app/uploaders/simple_file_uploader.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Simple file uploader without any processing
+class SimpleFileUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    slug = "#{model.id / 100.floor}/#{model.id}"
+
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{slug}"
+  end
+end
