cd14c1ef36e584674adeb0e5a14663f9d1665030
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-01-30 02:04:29 +0300

    Added spam trap to feedback requests

--- app/controllers/feedback_requests_controller.rb
+++ app/controllers/feedback_requests_controller.rb
@@ -1,21 +1,35 @@
+# frozen_string_literal: true
+
+# Processing feedback requests
 class FeedbackRequestsController < ApplicationController
   # post /feedback_requests
   def create
     @entity = FeedbackRequest.new(creation_parameters)
-    if @entity.save
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-        format.js
-      end
+    if params[:agree]
+      show_result
+    else
+      save_entity
+    end
+  end
 
+  private
+
+  def save_entity
+    if @entity.save
+      show_result
       FeedbackMailer.new_feedback_request(@entity.id).deliver_later
     else
       redirect_to root_path
     end
   end
 
-  private
+  def show_result
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { head :no_content }
+      format.js
+    end
+  end
 
   def creation_parameters
     parameters = params.require(:feedback_request).permit(FeedbackRequest.creation_parameters)
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.31.190130.0'
+    VERSION = '0.31.190130.1'
   end
 end
