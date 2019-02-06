94777ea71bbc71d0db3bd6421f10e86d545226a5
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-02-07 00:41:54 +0300

    Updated factories in sample specs

--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.31.190130.1'
+    VERSION = '0.31.190207.0'
   end
 end
--- sample/spec/factories/users.rb
+++ sample/spec/factories/users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
     sequence(:screen_name) { |n| "User#{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password 'secret'
-    password_confirmation 'secret'
+    password { 'secret' }
+    password_confirmation { 'secret' }
   end
 end
