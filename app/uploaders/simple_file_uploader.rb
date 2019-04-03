3c8614b350711d32a8586903ed9f756ad217d8cd
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-04-03 17:00:44 +0300

    Added blacklisted extensions for simple file uploader

--- app/uploaders/simple_file_uploader.rb
+++ app/uploaders/simple_file_uploader.rb
@@ -9,4 +9,19 @@ class SimpleFileUploader < CarrierWave::Uploader::Base
 
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{slug}"
   end
+
+  def extension_blacklist
+    %w[
+      htm html action apk app bat bin cmd com command cpl csh exe gadget inf ins
+      inx ipa isu job jse ksh lnk msc msi msp mst osx out paf pif prg ps1 reg
+      rgs run scr sct shb shs u3p vb vbe vbs vbscript workflow ws wsf wsh 0xe
+      73k 89k a6p ac acc acr actm ahk air app arscript as asb awk azw2 beam btm
+      cel celx chm cof crt dek dld dmc docm dotm dxl ear ebm ebs ebs2 ecf eham
+      elf es ex4 exopc ezs fas fky fpi frs fxp gs ham hms hpf hta iim ipf isp
+      jar js jsx kix lo ls mam mcr mel mpx mrc ms ms mxe nexe obs ore otm pex
+      plx potm ppam ppsm pptm prc pvd pwc pyc pyo qpx rbx rox rpj s2a sbs sca
+      scar scb script smm spr tcp thm tlb tms udf upx url vlx vpm wcm widget wiz
+      wpk wpm xap xbap xlam xlm xlsm xltm xqt xys zl9 dmg pkg php rb
+    ]
+  end
 end
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.34.190401.0'
+    VERSION = '0.34.190403.0'
   end
 end
