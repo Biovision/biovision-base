fa077aded09dc1f4380231852072c9d0fb8cf1e8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-05-07 16:24:46 +0300

    Better canonizer
    Using frozen string literal and transliteration map;
    Casting inputs to string.

--- app/services/canonizer.rb
+++ app/services/canonizer.rb
@@ -1,31 +1,38 @@
+# frozen_string_literal: true
+
+# Helper for canonizing and transliterating strings
 class Canonizer
+  # Keys are not latin letters
   TRANSLITERATION_MAP = {
-      'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e',
-      'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k',
-      'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
-      'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'kh', 'ц' => 'c',
-      'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
-      'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
-      'å' => 'ao', 'ä' => 'ae', 'ö' => 'oe', 'é' => 'e'
-  }
+    'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e',
+    'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k',
+    'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
+    'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'kh', 'ц' => 'c',
+    'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+    'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+    'å' => 'ao', 'ä' => 'ae', 'ö' => 'oe', 'é' => 'e'
+  }.freeze
 
   # @param [String] text
   def self.transliterate(text)
-    result = text.downcase
-    TRANSLITERATION_MAP.each { |r, e| result.gsub!(r, e) }
-    result.downcase.gsub(/[^-a-z0-9_]/, '-').gsub(/^[-_]*([-a-z0-9_]*[a-z0-9]+)[-_]*$/, '\1').gsub(/--+/, '-')
+    pattern = Regexp.new "[#{TRANSLITERATION_MAP.keys.join}]"
+    result = text.to_s.downcase.gsub(pattern, TRANSLITERATION_MAP)
+
+    a = /[^-a-z0-9_]/ # non-allowed characters will be replaced with dash
+    b = /\A[-_]*([-a-z0-9_]*[a-z0-9]+)[-_]*\z/ # chop leading and trailing dash
+    result.gsub(a, '-').gsub(b, '\1').gsub(/--+/, '-').gsub(/-+\z/, '')
   end
 
   # @param [String] input
   def self.canonize(input)
-    lowered   = input.downcase.strip
+    lowered = input.to_s.downcase.strip
     canonized = lowered.gsub(/[^a-zа-я0-9ё]/, '')
     canonized.empty? ? lowered : canonized
   end
 
   # @param [String] input
   def self.urlize(input)
-    lowered = input.downcase.squish
-    lowered.gsub(/[^a-zа-я0-9ё]/, '-').gsub(/-\z/, '')
+    lowered = input.to_s.downcase.squish
+    lowered.gsub(/[^a-zа-я0-9ё]/, '-').gsub(/-+\z/, '')
   end
 end
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.36.190506.1'
+    VERSION = '0.36.190507.0'
   end
 end
