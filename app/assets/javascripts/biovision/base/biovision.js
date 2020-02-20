30a43b446a2551bee6ccca3e3e63ad4be613f03a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2020-02-20 18:05:17 +0300

    Button for removing images

--- app/assets/javascripts/biovision/base/biovision.js
+++ app/assets/javascripts/biovision/base/biovision.js
@@ -618,6 +618,48 @@ Biovision.components.entityImageRemover = {
     }
 };
 
+Biovision.components.imageRemover = {
+    initialized: false,
+    selector: ".js-image-remover",
+    buttons: [],
+    init: function () {
+        document.querySelectorAll(this.selector).forEach(this.apply);
+        this.initialized = true;
+    },
+    apply: function (button) {
+        const component = Biovision.components.imageRemover;
+        component.buttons.push(button);
+        button.addEventListener("click", component.handler);
+    },
+    handler: function (event) {
+        const button = event.target;
+        const container = document.getElementById(button.getAttribute("data-container"));
+
+        if (container && confirm(button.getAttribute("data-text"))) {
+            const image = container.querySelector("img");
+            const url = button.getAttribute("data-url");
+            const request = Biovision.jsonAjaxRequest("delete", url, function () {
+                const response = JSON.parse(this.responseText);
+                if (response.hasOwnProperty("meta")) {
+                    const meta = response.meta;
+                    if (meta.hasOwnProperty("result") && meta.result) {
+                        image.src = button.getAttribute("data-blank-url");
+                        button.remove();
+
+                        const link = image.closest("a");
+                        if (link) {
+                            link.href = "#";
+                        }
+                    }
+                }
+            });
+
+            button.disabled = true;
+            request.send();
+        }
+    }
+};
+
 Biovision.components.ajaxDeleteButton = {
     initialized: false,
     selector: 'button.destroy[data-url]',
