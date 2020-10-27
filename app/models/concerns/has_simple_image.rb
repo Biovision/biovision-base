23b7ba95e6f772b58698a49fe23fd3208d0bba81
Maxim Khan-Magomedov <maxim.km@gmail.com>
2020-10-28 00:24:52 +0300

    Added simple image inclusion

--- app/models/concerns/has_simple_image.rb
+++ app/models/concerns/has_simple_image.rb
@@ -6,6 +6,9 @@ module HasSimpleImage
 
   included do
     belongs_to :simple_image, optional: true, counter_cache: :object_count
+
+    scope :joined_image, -> { left_joins(:simple_image) }
+    scope :included_image, -> { includes(:simple_image) }
   end
 
   # @return [SimpleImageUploader]
--- lib/biovision/base/version.rb
+++ lib/biovision/base/version.rb
@@ -2,6 +2,6 @@
 
 module Biovision
   module Base
-    VERSION = '0.51.201027.0'
+    VERSION = '0.51.201028.0'
   end
 end
